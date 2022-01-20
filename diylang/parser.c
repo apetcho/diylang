55ce28af90d0c1e2949c4ae4f862adbcf392f7de
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-20 13:09:17 +0100

    Implement initialize() function

--- diylang/parser.c
+++ diylang/parser.c
@@ -379,7 +379,11 @@ void print_ast(Tree *tree){
 
 // *** init_io() => initialize()
 void initialize(FILE **fp, FILE *std, const char mode[], const char filename[]){
-    // TODO
+    if(filename[0] == "\0"){
+        *fp = std;
+    }else if((*fp=fopen(filename, mode)) == NULL){
+        error(0, 0, "Can't open %s\n", filename);
+    }
 }
 
 
