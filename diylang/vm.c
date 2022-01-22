33ca1e99f55d5bd16d6a5e5eb363154d66eaec6c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-22 08:21:28 +0100

    Implement intepretation of LE instruction

--- diylang/vm.c
+++ diylang/vm.c
@@ -131,6 +131,10 @@ again:
         sp[-2] = sp[-2] > sp[-1];
         --sp;
         goto again;
+    case LE:
+        sp[-2] = sp[-2] <= sp[-1];
+        --sp;
+        goto again;
     }// switch
 }
 
